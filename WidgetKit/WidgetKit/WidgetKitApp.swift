f49cbb813bd69c61aa84f350202611382136e603
Késia Silva <kesiasilvab6@gmail.com>
2025-10-11 10:30:34 -0300

    feat: Criando novo projeto de Widgets

---
+++ WidgetKit/WidgetKit/WidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitApp.swift
+//  WidgetKit
+//
+//  Created by Késia Silva Viana on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
