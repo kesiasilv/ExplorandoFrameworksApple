006e364d32be8b031da311d720b9a0f100d1cd4b
Késia Silva <kesiasilvab6@gmail.com>
2025-10-12 09:15:23 -0300

    feat: New project of Push Notification

---
+++ StudyPushNotification/StudyPushNotification/StudyPushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPushNotificationApp.swift
+//  StudyPushNotification
+//
+//  Created by Késia Silva Viana on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyPushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
