ba6e1e07393fe4890c6732522d3689bff66e86a9
Késia Silva <kesiasilvab6@gmail.com>
2025-10-13 11:52:13 -0300

    feat: Testando o projeto do video apple

---
+++ TestFoundationsModels/TestFoundationsModels/TestFoundationsModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFoundationsModelsApp.swift
+//  TestFoundationsModels
+//
+//  Created by Késia Silva Viana on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TestFoundationsModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
